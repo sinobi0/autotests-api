a01f8f711d4ab889e7069ba94cfdc5ba49d34632
sinobi0 <322lex@gmail.com>
2025-11-30 23:20:54 +0300

    6.5 Практикуемся в использовании API клиентов

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -1,6 +1,22 @@
-from clients.api_client import APIClient
 from typing import TypedDict
+
 from httpx import Response
+
+from clients.api_client import APIClient
+from clients.public_http_builder import get_public_http_client
+
+
+# Добавили описание структуры пользователя
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
 
 class CreateUserRequestDict(TypedDict):
     """
@@ -12,14 +28,39 @@ class CreateUserRequestDict(TypedDict):
     firstName: str
     middleName: str
 
+
+# Добавили описание структуры ответа создания пользователя
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
+
+
 class PublicUsersClient(APIClient):
     """
     Клиент для работы с /api/v1/users
     """
+
     def create_user_api(self, request: CreateUserRequestDict) -> Response:
         """
-        Метод выполняет создание пользователя.
-        :param request: Словарь с необходимыми данными для создания
+        Метод создает пользователя.
+
+        :param request: Словарь с email, password, lastName, firstName, middleName.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/users", json=request)
+
+    # Добавили новый метод
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
+
+
+def get_public_users_client() -> PublicUsersClient:
+    """
+    Функция создаёт экземпляр PublicUsersClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию PublicUsersClient.
+    """
+    return PublicUsersClient(client=get_public_http_client())
