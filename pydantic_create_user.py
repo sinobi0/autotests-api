48412ddb2df8dee0eb6cfd6ecccbd2afd234f5e1
sinobi0 <322lex@gmail.com>
2025-12-04 23:17:26 +0300

    7.1 Знакомство с Pydantic. Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,33 @@
+import uuid
+
+from pydantic import BaseModel, Field, EmailStr, constr
+
+from tools.fakers import get_random_email
+
+
+class UserSchema(BaseModel):
+    """
+    Структура описания пользователя
+    """
+    id: str
+    email: EmailStr = Field(default_factory= lambda: get_random_email())
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName",default="Mike")
+    middle_name: str = Field(alias="middleName",default="Test")
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Структура описания запроса создания пользователя
+    """
+    email: EmailStr = Field(default_factory= lambda: get_random_email())
+    password: constr(min_length=6)
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName",default="Mike")
+    middle_name: str = Field(alias="middleName",default="Test")
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Структура описания ответа на запрос создания пользователя
+    """
+    user: UserSchema
+
