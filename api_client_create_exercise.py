27157513d4b84f6255ef379b5336920a712ecce7
sinobi0 <322lex@gmail.com>
2025-12-05 21:52:33 +0300

    Применяем Pydantic в API клиентах

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -1,33 +1,33 @@
 from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
 from clients.exercises.exercises_client import get_exercises_client, CreateExerciseRequestDict
-from clients.files.files_client import get_files_client, CreateFileRequestDict
-from clients.private_http_builder import AuthenticationUserDict
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from clients.files.files_client import get_files_client, CreateFileRequestSchema
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.public_users_client import get_public_users_client, CreateUserRequestSchema
 from tools.fakers import get_random_email
 
 #Инициализируем публичный клиент и создаем пользователя
 public_users_client = get_public_users_client()
-create_user_req = CreateUserRequestDict(
+create_user_req = CreateUserRequestSchema(
     email=get_random_email(),
     password="string",
-    lastName="string",
-    firstName="string",
-    middleName="string"
+    last_name="string",
+    first_name="string",
+    middle_name="string"
 )
 create_user_response = public_users_client.create_user(create_user_req)
 print(f"Данные созданного пользователя: {create_user_response}")
 
 # Инициализируем клиенты
-authentication_user = AuthenticationUserDict(
-    email=create_user_req['email'],
-    password=create_user_req['password']
+authentication_user = AuthenticationUserSchema(
+    email=create_user_req.email,
+    password=create_user_req.password
 )
 files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 exercises_client = get_exercises_client(authentication_user)
 
 #Загружаем файл
-create_file_req = CreateFileRequestDict(
+create_file_req = CreateFileRequestSchema(
     filename="image_3.jpg",
     directory="test115",
     upload_file="./testdata/files/image_3.jpg"
@@ -42,8 +42,8 @@ create_course_req = CreateCourseRequestDict(
     minScore=10,
     description="test",
     estimatedTime="115",
-    previewFileId=create_file_response['file']['id'],
-    createdByUserId=create_user_response['user']['id']
+    previewFileId=create_file_response.file.id,
+    createdByUserId=create_user_response.user.id
 )
 create_course_response = courses_client.create_course(create_course_req)
 print(f"Данные созданного курса: {create_course_response}")
--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -2,7 +2,7 @@ from clients.api_client import APIClient
 from typing import TypedDict
 from httpx import Response
 
-from clients.private_http_builder import get_private_http_client, AuthenticationUserDict
+from clients.private_http_builder import get_private_http_client, AuthenticationUserSchema
 
 
 
@@ -142,7 +142,7 @@ class ExercisesClient(APIClient):
         response = self.update_exercise_api(exercise_id, request)
         return response.json()
 
-def get_exercises_client(user: AuthenticationUserDict) -> ExercisesClient:
+def get_exercises_client(user: AuthenticationUserSchema) -> ExercisesClient:
     """
     Функция создаёт экземпляр ExercisesClient с уже настроенным HTTP-клиентом.
 
