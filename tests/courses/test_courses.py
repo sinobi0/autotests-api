83a0e478492225cb7adcf60a9c2509cb15089d62
sinobi0 <322lex@gmail.com>
2026-01-27 21:31:28 +0300

    9.3 Практикуемся в написании API автотестов. Часть 2. Практика написания API автотестов для работы с курсами

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -3,10 +3,13 @@ from http import HTTPStatus
 import pytest
 
 from clients.courses.courses_client import CoursesClient
-from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema
+from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema, CreateCourseRequestSchema, \
+    CreateCourseResponseSchema
 from fixtures.courses import CourseFixture
+from fixtures.files import FileFixture
+from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.courses import assert_update_course_response
+from tools.assertions.courses import assert_update_course_response, assert_create_course_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -28,3 +31,20 @@ class TestCourses:
 
         # Валидируем JSON-схему ответа
         validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_create_course(
+            self,
+            courses_client: CoursesClient,
+            function_file: FileFixture,
+            function_user: UserFixture
+    ):
+        request = CreateCourseRequestSchema(
+            preview_file_id= function_file.response.file.id,
+            created_by_user_id= function_user.response.user.id
+        )
+        response = courses_client.create_course_api(request)
+        response_data = CreateCourseResponseSchema.model_validate_json(response.text)
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_create_course_response(request, response_data)
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
--- tools/assertions/courses.py
+++ tools/assertions/courses.py
@@ -1,9 +1,27 @@
 from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema, CourseSchema, \
-    GetCoursesResponseSchema, CreateCourseResponseSchema
+    GetCoursesResponseSchema, CreateCourseResponseSchema, CreateCourseRequestSchema
 from tools.assertions.base import assert_equal, assert_length
 from tools.assertions.files import assert_file
 from tools.assertions.users import assert_user
 
+def assert_create_course_response (
+        request: CreateCourseRequestSchema,
+        response: CreateCourseResponseSchema
+):
+    """
+    Проверяет, что ответ на создание курса соответствует данным запроса
+    :param request: Исходный запрос создания курса
+    :param response: Ответ от API созданного курса
+    :return: raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(response.course.title, request.title, "title")
+    assert_equal(response.course.max_score, request.max_score, "max_score")
+    assert_equal(response.course.min_score, request.min_score, "min_score")
+    assert_equal(response.course.description, request.description, "description")
+    assert_equal(response.course.estimated_time, request.estimated_time, "estimatedTime")
+
+    assert_equal(response.course.preview_file.id, request.preview_file_id, "preview_file_id")
+    assert_equal(response.course.created_by_user.id, request.created_by_user_id, "created_by_user_id")
 
 def assert_update_course_response(
         request: UpdateCourseRequestSchema,
