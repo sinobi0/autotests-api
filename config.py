ec15888bbbe97fec114d97d8263bce25b0a462a3
Михаил Костин <322lex@gmail.com>
2026-03-28 16:57:58 +0300

    Added run autotests on CI

--- config.py
+++ config.py
@@ -17,6 +17,7 @@ class TestDataConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
+        extra='allow',
         env_file=".env",  # Указываем, из какого файла читать настройки
         env_file_encoding="utf-8",  # Указываем кодировку файла
         env_nested_delimiter=".",  # Указываем разделитель для вложенных переменных
